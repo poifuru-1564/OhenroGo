3c305e00feb7613dde84532b237f6c3f12e47f63
Mei <mei.hiraoka0731@gmail.com>
2025-10-31 17:21:56 +0900

    edit blogListing page

---
+++ resources/views/blogListing.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+     <div class="prefecture-category">
+     <div class="location-category">
+     <div class="status-category">
+     <div class="location-category">
+     <div class="topic-category">
+     <div class="user-category">
+    
+
+
