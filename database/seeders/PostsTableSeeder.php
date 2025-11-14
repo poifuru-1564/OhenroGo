f046ab051543506efd72dabe6bd6476707d57e38
Mei <mei.hiraoka0731@gmail.com>
2025-11-14 14:17:13 +0900

    Refactor PostsTableSeeder to use specific user and topic IDs, update post details for realistic seeding

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -21,87 +21,15 @@ class PostsTableSeeder extends Seeder
 
         DB::table('posts')->insert([
             
-            [
-                'user_id' => User::where('role_id', 1)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Temple')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' => 'iwamotoji_37_ceiling.png',
-                'title' => '575 paintings on the ceiling!',
-                'created_at' => now(),
-                'prefecture_id' =>DB::table('prefectures')->where('name', 'Kochi')->value('id'),
-                'temple_id'=> DB::table('temples')->where('id', '37')->value('id'),
-            ],
-
-            [
-                'user_id' => User::where('role_id', 2)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Temple')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' =>'img/ryozenji.png',
-                'title' => 'Ryozenji Temple',
-                'created_at' => now(),
-                'prefecture_id' =>DB::table('prefectures')->where('name', 'Tokushima')->value('id'),
-                'temple_id'=> DB::table('temples')->where('id', '1')->value('id'),
-            ],
-
-            
-            [
-                'user_id' => User::where('role_id', 1)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Temple')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' =>'img/stamp_okuboji_88.png',
-                'title' => 'Goshuin from the final temple',
-                'created_at' => now(),
-                'prefecture_id' =>DB::table('prefectures')->where('name', 'Kagawa')->value('id'),
-                'temple_id'=> DB::table('temples')->where('id', '88')->value('id'),
-            ],
-           
-            [
-                'user_id' => User::where('role_id', 1)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Food')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' =>'img/udon_shosanji.png',
-                'title' => 'Best udon in Tokushima',
-                'created_at' => now(),
-                'prefecture_id' =>DB::table('prefectures')->where('name', 'Tokushima')->value('id'),
-                'temple_id'=> DB::table('temples')->where('id', '12')->value('id'),
-            ],
-
-
-            [
-                'user_id' => User::where('role_id', 3)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Other')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'Before Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'title' => 'Arrived in Tokushima!',
-                'photo_path' => null,
-                'created_at' => now(),
-                'prefecture_id' =>DB::table('prefectures')->where('name', 'Tokushima')->value('id'),
-                'temple_id'=> DB::table('temples')->where('id', '1')->value('id'),
-            ],
-            [
-                'user_id' => User::where('role_id', 2)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Support (Osettai)')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'Before Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'title' => 'Things to pack for an Shikoku Ohenro',
-                'photo_path' => null,
-                'created_at' => now(),
-                'prefecture_id' =>DB::table('prefectures')->where('name', 'Tokushima')->value('id'),
-                'temple_id'=> DB::table('temples')->where('id', '1')->value('id'),
-            ],
 
             ## Realistic posts for seeding the database
             [
                 'user_id' => DB::table('users')->where('id', 1)->value('id'),
                 'topic_id' => DB::table('topics')->where('id', 4)->value('id'),
-                'status_id' => DB::table('status')->where('id', 2)->value('id'),
+                'status_id' => DB::table('status')->where('id', 1)->value('id'),
                 'body' => "I started walking the Shikoku pilgrimage route today, but the sun was unbelievably strong and I honestly felt like I might get heatstroke. The water I brought even turned warm, so I spent a lot of time searching for vending machines and convenience stores as I walked. I also realized that because I’m a bit stooped, carrying a backpack makes my shoulders tense up. Using trekking poles helps keep my back straight, so I’ll walk with them from now on.\nWhen the temples are close together, I can take breaks and it’s not too exhausting, but when the distance stretches to about 5 km, it gets really tough with all the asphalt roads and uphill climbs. Still, I’m glad I managed to reach my target temple today. I’m so grateful to the people who offered me ice water and bananas, to the fellow pilgrims, and to the people who reached out when they heard I had started the pilgrimage. Thanks to all of you, I made it. Your support meant a lot to me.",
-                'title' => 'The first day of Ohenro',
-                'photo_path' => null,
+                'photo_path' => 'img/for_seed/ryozenji_1.png',
+                'title' => "The first day of Ohenro",
                 'created_at' => now(),
                 'prefecture_id' => DB::table('prefectures')->where('id', 1)->value('id'),
                 'temple_id' => DB::table('temples')->where('id', 1)->value('id'),
@@ -112,31 +40,29 @@ class PostsTableSeeder extends Seeder
                 'topic_id' => DB::table('topics')->where('id', 1)->value('id'),
                 'status_id' => DB::table('status')->where('id', 1)->value('id'),
                 'body' => "I had udon after visiting the temple, and it was so good I was honestly emotional. Even the water the lady at the udon shop gave me tasted amazing. I was just overwhelmed with gratitude that I managed to reach Shozanji today.",
-                'photo_path' => 'img/udon_shosanji_12.png',
+                'photo_path' => 'img/for_seed/udon_shosanji_12.png',
                 'title' => "Udon at Shosanji Temple",
                 'created_at' => now(),
                 'prefecture_id' => DB::table('prefectures')->where('id', 1)->value('id'),
                 'temple_id' => DB::table('temples')->where('id', 12)->value('id'),
             ],
-
             [
                 'user_id' => DB::table('users')->where('id', 4)->value('id'),
                 'topic_id' => DB::table('topics')->where('id', 1)->value('id'),
                 'status_id' => DB::table('status')->where('id', 2)->value('id'),
                 'body' => "Dinner",
-                'photo_path' => 'img/dinner.png',
+                'photo_path' => 'img/for_seed/dinner.png',
                 'title' => "Dinner in Tokushima",
                 'created_at' => now(),
                 'prefecture_id' => DB::table('prefectures')->where('id', 1)->value('id'),
                 'temple_id' => DB::table('temples')->where('id', 23)->value('id'),
             ],
-
             [
                 'user_id' => DB::table('users')->where('id', 6)->value('id'),
                 'topic_id' => DB::table('topics')->where('id', 4)->value('id'),
                 'status_id' => DB::table('status')->where('id', 3)->value('id'),
                 'body' => "This is the stamp from the last temple! Thanks Ohenro!",
-                'photo_path' => 'img/stamp_okuboji_88.png',
+                'photo_path' => 'img/for_seed/stamp_okuboji_88.png',
                 'title' => "The stamp at Koyasan (The last temple in Ohenro)",
                 'created_at' => now(),
                 'prefecture_id' => DB::table('prefectures')->where('id', 4)->value('id'),
@@ -148,30 +74,30 @@ class PostsTableSeeder extends Seeder
                 'topic_id' => DB::table('topics')->where('id', 5)->value('id'),
                 'status_id' => DB::table('status')->where('id', 1)->value('id'),
                 'body' => "First seaside view I have seen in Ohenro!! Goodbye Tokushima!",
-                'photo_path' => 'img/seaside.png',
+                'photo_path' => 'img/for_seed/seaside.png',
                 'title' => "Seaside View",
                 'created_at' => now(),
                 'prefecture_id' => DB::table('prefectures')->where('id', 1)->value('id'),
                 'temple_id' => DB::table('temples')->where('id', 23)->value('id'),
             ],
+
             [
                 'user_id' => DB::table('users')->where('id', 8)->value('id'),
                 'topic_id' => DB::table('topics')->where('id', 3)->value('id'),
                 'status_id' => DB::table('status')->where('id', 1)->value('id'),
                 'body' => "Thank you for the kindness and warm gifts! At 85 temple.",
-                'photo_path' => 'img/gift_from_local.png',
+                'photo_path' => 'img/for_seed/gift_from_local.png',
                 'title' => "Gift by Locals",
                 'created_at' => now(),
                 'prefecture_id' => DB::table('prefectures')->where('id', 4)->value('id'),
                 'temple_id' => DB::table('temples')->where('id', 85)->value('id'),
             ],
-
             [
                 'user_id' => DB::table('users')->where('id', 9)->value('id'),
                 'topic_id' => DB::table('topics')->where('id', 4)->value('id'),
                 'status_id' => DB::table('status')->where('id', 2)->value('id'),
                 'body' => "Iwamotoji Temple",
-                'photo_path' => 'img/iwamotoji_37.png',
+                'photo_path' => 'img/for_seed/iwamotoji_37.png',
                 'title' => "Iwamotoji temple",
                 'created_at' => now(),
                 'prefecture_id' => DB::table('prefectures')->where('id', 2)->value('id'),
@@ -200,7 +126,6 @@ class PostsTableSeeder extends Seeder
                 'prefecture_id' => DB::table('prefectures')->where('id', 4)->value('id'),
                 'temple_id' => DB::table('temples')->where('id', 88)->value('id'),
             ],
-
             [
                 'user_id' => DB::table('users')->where('id', 10)->value('id'),
                 'topic_id' => DB::table('topics')->where('id', 4)->value('id'),
@@ -223,7 +148,6 @@ class PostsTableSeeder extends Seeder
                 'prefecture_id' => DB::table('prefectures')->where('id', 4)->value('id'),
                 'temple_id' => DB::table('temples')->where('id', 66)->value('id'),
             ],
-
             [
                 'user_id' => DB::table('users')->where('id', 12)->value('id'),
                 'topic_id' => DB::table('topics')->where('id', 4)->value('id'),
@@ -235,105 +159,8 @@ class PostsTableSeeder extends Seeder
                 'prefecture_id' => DB::table('prefectures')->where('id', 2)->value('id'),
                 'temple_id' => DB::table('temples')->where('id', 29)->value('id'),
             ],
+
         ]);
-
-        $posts = [
-            [
-                'user_id' => User::where('role_id', 1)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Other')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'After Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' => 'img/for_seed/after_ohenro.png',
-                'title' => 'Ohenro Completed!',
-                'created_at' => now(),
-            ],
-            [
-                'user_id' => User::where('role_id', 3)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Food')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path'=>'img/for_seed/dinner.png',
-                'title' => 'Traditional Japanese Dinner',
-                'created_at' => now(),
-            ],
-            [
-                'user_id' => User::where('role_id', 1)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Support (Osettai)')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' => 'img/gift_from_local.png',
-                'title' => 'A lovely gift from a local',
-                'created_at' => now(),
-            ],
-
-            [
-                'user_id' => User::where('role_id', 2)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Other')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' =>'img/museum_1.png',
-                'title' => 'Museum 1',
-                'created_at' => now(),
-            ],
-
-            [
-                'user_id' => User::where('role_id', 1)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Ohenro road')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' =>'img/road.png',
-                'title' => 'Tiny trail art on the way to Temple No.50',
-                'created_at' => now(),
-            ],
-
-            [
-                'user_id' => User::where('role_id', 1)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Ohenro road')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' =>'img/seaside.png',
-                'title' => 'Beautiful seaside view',
-                'created_at' => now(),
-            ],
-             [
-                'user_id' => User::where('role_id', 3)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Hotel & Inn')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' =>'img/temple_inn.png',
-                'title' => 'hotel 1',
-                'created_at' => now(),
-            ],
-            [
-                'user_id' => User::where('role_id', 2)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Food')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' => null,
-                'title' => 'Must-buy souvenirs in Shikoku',
-                'created_at' => now(),
-            ],
-            [
-                'user_id' => User::where('role_id', 3)->inRandomOrder()->value('id'),
-                'topic_id'=>DB::table('topics')->where('name', 'Support (Osettai)')->value('id'),
-                'status_id'=>DB::table('status')->where('name', 'During Ohenro')->value('id'),
-                'body' => fake()->paragraph(),
-                'photo_path' => null,
-                'title' => 'Free public onsen (hot spring) near our hotel',
-                'created_at' => now(),
-            ]
-
-        ];
-
-        foreach ($posts as &$post)
-        {
-            $temple = Temple::inRandomOrder()->first();
-
-            $post['temple_id'] = $temple->id;
-            $post['prefecture_id'] = $temple->prefecture_id;
-        }
-
-        DB::table('posts')->insert($posts);
 
     }
 }
